5ab294f970d296a061686aa61fc2c0891993104f
Alexander Lomakin <alom2222@gmail.com>
2015-09-24 20:43:31 +0300

    lab8 version 1.3

--- lab8/main.c
+++ lab8/main.c
@@ -3,12 +3,25 @@
 
 int main()
 {
-	int n;
-	//long long d[100000];
+	int n = -1, check = 0;
 	int p[7] = { 2, 5, 10, 20, 50, 100, 200 };
 
-	scanf("%d", &n);
-	int* d = (int)malloc(sizeof(long long) * n);
+	while (!check)
+	{
+		printf("Input the amount of money in pennies: ");
+		scanf("%d", &n);
+		scanf("%*[^\n]");
+		if (n < 0)  {
+			printf("incorrect input, try again\n");
+		}
+		else
+		{
+			check = 1;
+		}
+
+	}
+	
+	int* d = (int)malloc(sizeof(int) * (n + 1));
 
 	for (int i = 0; i <= n; i++)
 	{
@@ -30,4 +43,5 @@ int main()
 
 	printf("%d", d[n]);
 	free(d);
+	_getch();
 }
