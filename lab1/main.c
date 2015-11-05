b33b761b3dbf68356c853aba0658150561bcb2d4
Platonova Mariia <maria1097@yandex.ru>
2015-11-05 15:13:32 +0300

    some changes

--- lab1/main.c
+++ lab1/main.c
@@ -3,12 +3,12 @@
 #include "math.h"
 #include "string.h"
 
-void printBinaryRepresentation(char*a,int k)
+void printBinaryRepresentation(char *a,int k)
 {
 	
-	for (int i =k-1; i >=0; i--)
+	for (int i = k - 1; i >= 0; i--)
 	{
-		for (int j = 7; j>=0 ; j--)
+		for (int j = 7; j >= 0; j--)
 		{
 			printf("%d", (a[i] >> j) & 1);
 		}
@@ -20,27 +20,27 @@ void printBinaryRepresentation(char*a,int k)
 void BinInt(int n) // Представление в виде int32.
 {
 	int size = sizeof(int);
-	char* a = (char*)(&n);
+	char *a = (char *)(&n);
 	printBinaryRepresentation(a, size);
 }
 
 void BinFloat(float n) // Представление в виде float.
 {
 	int size = sizeof(float);
-	char* a = (char*)(&n);
-	printBinaryRepresentation(a,size);
+	char *a = (char *)(&n);
+	printBinaryRepresentation(a, size);
 }
 
 void BinDouble(double n) // Представление в виде double.
 {
 	int size = sizeof(double);
-	char* a = (char*)(&n);
-	printBinaryRepresentation(a,size);
+	char *a = (char*)(&n);
+	printBinaryRepresentation(a, size);
 }
 
 int main()
 {
-	int n = strlen("Platonova")* strlen("Mariia")* strlen("Victorovna");
+	int n = strlen("Platonova") * strlen("Mariia") * strlen("Victorovna");
 	printf("The result of multiplication: ");
 	printf("%d\n", n);
 	printf("Int32 representation: ");
