ca7c0f123b6e6d53a051edc043d23314ed747896
kulikovpd <black.angel9121@gmail.com>
2015-11-30 21:45:51 +0300

    fix lab2

--- lab_2_1/lab_2.c
+++ lab_2_1/lab_2.c
@@ -4,7 +4,7 @@
 
 int gcd(int a, int b)
 {
-	return (a) ? gcd(b%a, a) : b;
+	return (a) ? gcd(b % a, a) : b;
 }
 
 int main()
@@ -17,6 +17,7 @@ int main()
 	{
 		printf("Enter the numbers\n");
 		scanf_s("%f%f%f", &x, &y, &z);
+		scanf_s("%*[^\n]");
 		if (x <= 0 || y <= 0 || z <= 0 || x - floor(x) > g || y - floor(y) > g || z - floor(z) > g)
 		{
 			printf("Incorrect input\n");
@@ -30,21 +31,21 @@ int main()
 			f = 1;
 		}
 	}
-		if ((a*a + b*b == c*c) || (b*b + c*c == a*a) || (a*a + c*c == b*b))
+	if ((a * a + b * b == c * c) || (b * b + c * c == a * a) || (a *a  + c * c == b * b))
+	{
+		if (gcd(gcd(a, b), c) == 1)
 		{
-			if (gcd(gcd(a, b), c) == 1)
-			{
-				printf("Primitive pythagorean triple\n");
-			}
-			else
-			{
-				printf("Nonprimitive pythagorean triple\n");
-			}
+			printf("Primitive pythagorean triple\n");
 		}
 		else
 		{
-			printf("Not a pythagorean triple\n");
+			printf("Nonprimitive pythagorean triple\n");
 		}
+	}
+	else
+	{
+			printf("Not a pythagorean triple\n");
+	}
 	getchar();
 	return 0;
 }
