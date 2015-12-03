761c7b82259318a25ecf4e7e1c52ba1b2d19dcb4
Nadya <hikari.aka.nadya@yandex.ru>
2015-12-03 18:05:52 +0300

    lab3

--- Autumn/lab3/main.c
+++ Autumn/lab3/main.c
@@ -5,44 +5,39 @@
 
 void grad(double x, double y, double z)
 {
-	double cosA, minA, secA;
+	double A, cosA;
 	cosA = acos((pow(y, 2) + pow(z, 2) - pow(x, 2)) / (2 * y * z))* 180.0 / PI;
-	int g1 = (int)cosA;
-	minA = ((cosA - g1) * 60);
-	int g = (int)minA;
-	secA = ((minA - g) * 60);
-	int g2 = (int)secA;
 
-	printf("%d  %d' %d''\n", g1, g, g2);
+	double minA, secA;
+	int  intA = (int)cosA;
+	minA = ((cosA - intA) * 60);
+	int intmin = (int)minA;
+	secA = ((minA - intmin) * 60);
+	int intsec = (int)secA;
+
+	printf("angle :%d° %d' %d'' \n", intA, intmin, intsec);
 }
 
 int main()
 {
 	float a = 0, b = 0, c = 0;
-	char s[250];
-	printf("ddtlbnt lkbys cnjhjy\n");
-	int flag = 0;
-	while (flag == 0)
-	{
-		fgets(s, 80, stdin);
-		sscanf(s, "%f%*[' ']%f%*[' ']%f%*[' ']", &a, &b, &c);
+	printf("Enter three numbers.\n");
+	scanf("%f%*[' ']%f%*[' ']%f%*[' ']", &a, &b, &c);
 	if (a > 0 && b > 0 && c > 0)
 	{
-		flag = 1;
 		if (a < b + c && b < a + c && c < a + b)
 		{
-			printf("Треугольник существует.\n");
+			printf("Triangle exists.\n");
 			grad(a, b, c);
 			grad(b, c, a);
 			grad(c, a, b);
 		}
 		else
-			printf("Треугольник не существует.\n");
+			printf("Triangle is not exists.\n");
 	}
 	else
 	{
-		printf("error. again\n");
-	};
+		printf("Input Error. Try again.\n");
 	};
 
 	return 0;
