fab4f55074a178612b8dcf773fcd72c4dc866404
Mariia Platonova <maria1097@yandex.ru>
2015-12-23 22:03:56 +0300

    lab12_3

--- lab12/main.c
+++ lab12/main.c
@@ -2,84 +2,109 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MEMORY_SIZE 404
+#define MEMORY_SIZE 4040
 
 
-void *memoryStart;
-void *buffer;
-int *memoryInfo;
+void *memory;
 
 
 void init()
 {
-	memoryStart = malloc(MEMORY_SIZE);
-	buffer = malloc(MEMORY_SIZE);
-	memoryInfo = malloc(sizeof(int) * MEMORY_SIZE);
-
-	for (int i = 0; i < MEMORY_SIZE; ++i)
-		memoryInfo[i] = 0;
-
+	memory = malloc(MEMORY_SIZE);
+	memset(memory, 0, MEMORY_SIZE);
 }
 
 void *myMalloc(int size)
 {
-	int indexOfLastZero = -1;
-	for (int i = 0; i < MEMORY_SIZE; ++i)
+	if (*((int*)memory) == 0)
 	{
-		if (indexOfLastZero != -1 && (i - indexOfLastZero) == size)
+		*((int*)memory) = size;
+		return (void*)((char*)memory + 4);
+	}
+	else
+	{
+		char* current = memory;
+		while (1)
 		{
-			for (int j = 0; j < size; ++j)
-				memoryInfo[j + i - size] = 1;
-			memoryInfo[i - size] = size;
-			return (void*)((char*)memoryStart + i - size);
-		}
-		if (memoryInfo[i] == 0)
-		{
-			if (indexOfLastZero == -1)
-				indexOfLastZero = i;
-		}
-		else
-		{
-			indexOfLastZero = -1;
+			int blockSize = *((int*)current);
+			if (blockSize == 0)
+			{
+				if (current - (char*)memory + size >= MEMORY_SIZE)
+					return NULL;
+
+				int isGoodBlock = 1;
+
+				for (int i = 4; i < size + 4; i += 4)
+				{
+					if (*(int*)(current + i) != 0)
+					{
+						isGoodBlock = 0;
+						break;
+					}
+				}
+				if (isGoodBlock)
+				{
+					*((int *)current) = size;
+					return (void*)(current + 4);
+				}
+			}
+			current += blockSize + 4;
 		}
 	}
-
 	return NULL;
 }
 
+
 void myFree(void *data)
 {
-	int index = (int)((char*)data - memoryStart);
-	int size = memoryInfo[index];
-	for (int i = 0; i < size; ++i)
-		memoryInfo[index + i] = 0;
+	int size = *(int*)((char*)data - 4);
+	*(int*)((char*)data - 4) = 0;
+	for (int i = 0; i < size; i++)
+	{
+		*((char*)data + i) = 0;
+	}
+	*(int*)((char*)data - 4) = 0;
 }
+
 
 void *myRealloc(void *data, int size)
 {
-	memcpy(buffer, data, size);
+	int dataSize = *((int*)((char*)data - 4));
+
+	int flag = 1;
+
+	for (int i = 0; i < dataSize - size; i += 4)
+	{
+		if (*(int*)((char*)data + size + i) != 0)
+		{
+			flag = 0;
+			break;
+		}
+	}
+	if (flag)
+	{
+		*((int*)((char*)data - 4)) = size;
+		return data;
+	}
+
+	void* newData = myMalloc(size);
+	memcpy(newData, data, dataSize);
 	myFree(data);
-
-	int *startPointer = myMalloc(size);
-	if (startPointer == NULL)
-		return NULL;
-
-	memcpy(startPointer, buffer, size);
-	return startPointer;
-
-
+	return newData;
 }
+
 
 int main()
 {
 	init();
 	int *array = (int *)myMalloc(50 * sizeof(int));
 	for (int i = 0; i < 50; ++i)
-		array[i] = i + 1;
+		array[i] = i;
 
-	array = (int *)myRealloc(array, 100 * sizeof(int));
-	for (int i = 50; i < 100; ++i)
-		array[i] = 1234;
+	array = (int*)myRealloc(array, 70 * sizeof(int));
+
+	for (int i = 50; i < 70; ++i)
+		array[i] = 21;
 
 	myFree(array);
 
