79a1042febbb0d681414e1b67aa10ef456759855
Nadya <hikari.aka.nadya@yandex.ru>
2016-02-10 17:14:21 +0300

    lab5

--- Autumn/lab5/main.c
+++ Autumn/lab5/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <math.h> 
 
-void period_length(int n) 
+void period_length(int n)
 {
 	int a_0, a, b, c, b_0, c_0, result = 0;
 	a_0 = sqrt(n * 1.0);
 	b = b_0 = a_0;
 	c = c_0 = n - a_0 * a_0;
 	printf_s("Numbers: \n");
-	do 
+	do
 	{
 		a = (a_0 + b) / c;
 		printf("%d\n", a);
@@ -26,11 +26,11 @@ int main()
 	int flag = 0;
 	char c[80], c2[80];
 
-while (flag == 0)
+	while (flag == 0)
 	{
 		fgets(c, 80, stdin);
 		sscanf(c, "%d", &n);
-		if (strlen(c) == strlen(itoa(n, c2 , 10))+1)
+		if (strlen(c) == strlen(itoa(n, c2, 10)) + 1)
 		{
 			flag = 1;
 			period_length(n);
