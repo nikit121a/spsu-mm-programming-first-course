2b7ce02f832fead709e2346c4bcaccdd2b54bcc5
Nadya <hikari.aka.nadya@yandex.ru>
2016-02-10 15:41:29 +0300

    zombi!!

--- Autumn/zombi/main.c
+++ Autumn/zombi/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include < math.h > 
+#include <math.h> 
 
 typedef struct  TheThing{
 	int radius;
@@ -17,8 +17,8 @@ void zombiskill(TheThing *zombi, int i, int k1, int k2, int k3)
 {
 	zombi[i].health = 0;
 	zombi[i].immunity = 0;
-	zombi[i].virus = rand() % 101;
-	zombi[i].radius = rand() % 10;
+	zombi[i].virus = rand() % 101+1;
+	zombi[i].radius = rand() % 10+1;
 	zombi[i].life = k3 / (k1*zombi[i].radius + k2*zombi[i].virus);
 }
 
@@ -26,7 +26,7 @@ void zombiskill(TheThing *zombi, int i, int k1, int k2, int k3)
 void humanskill(TheThing *human, int i)
 {
 	human[i].health = 100;
-	human[i].immunity = rand() % 101;
+	human[i].immunity = rand() % 101+1;
 	human[i].virus = 0;
 	human[i].radius = 0;
 	human[i].life = 0;
@@ -36,60 +36,66 @@ void humanskill(TheThing *human, int i)
 int main()
 {
 	int i = 0;
-	int x, y, kolzombi, kolhuman = 950;
-	printf("hfpvths gjkz, zombi");
-	scanf("%d%d%d%", &x, &y, &kolzombi);
+	int x, y, kolzombi;
+	printf("Enter two numbers: the size of the square of the field and the number of zombies\n");
+	scanf("%d%d", &x, &kolzombi);
+	y = x;
+	int kolzombinew = kolzombi;
 
 	//массивы структур зомби и людей
+	// int * 7 элементов структуры thething
 	TheThing *TheThing;
-	TheThing = malloc(sizeof(TheThing) * 1000);
+	TheThing = malloc(28 * 1000);
 
 	//размещаем наших зомби
 	int **pole = (int**)malloc(x*sizeof(int*));
-	
+
 	// ¬вод элементов массива
 	//инициализаци€
-	for (i = 0; i < x; i ++)  // цикл по строкам
+	for (i = 0; i < x; i++)  // цикл по строкам
 	{
 		// ¬ыделение пам€ти под хранение строк
 		pole[i] = (int*)malloc(y*sizeof(int));
-		for (int j = 0; j < y; j ++)  // цикл по столбцам
-					pole[i][j] = -1;
+		for (int j = 0; j < y; j++)  // цикл по столбцам
+			pole[i][j] = -1;
 	}
 	////////////////////////////////////////////////////////////////////////
-	int period = floor((x*y) / 1000);
+	int period = sqrt((x*y) / 1000);
 	//расставить 1000 человек.
 	int k = 0;
 	for (i = 0; i < x; i += period)  // цикл по строкам
 		for (int j = 0; j < y; j += period)  // цикл по столбцам
-	{	
-		pole[i][j] = k; 
-		k++;
-		TheThing[k].x = i;
-		TheThing[k].y = j;
-	}
+		{
+			if (k == 1000) { break; break; }
+			pole[i][j] = k;
+			TheThing[k].x = i;
+			TheThing[k].y = j;
+			k++;
+		}
 
 
 	//нова€ попул€ци€
 	int *population = (int*)malloc(kolzombi * sizeof(int));
-	int k1 = rand() % 101, k2 = rand() % 101, k3 = rand() % 101;
+	int k1 = rand() % 101+1, k2 = rand() % 101+1, k3 = rand() % 101+1;
 	int createvirus = 0;
 
 	while (createvirus <= 0)
 	{
+		kolzombi = kolzombinew;
+		int kolhuman = 1000 - kolzombinew;
 		//устанавливаем всем люд€м скилл
 		for (i = 0; i < 1000; i++)
 		{
 			humanskill(TheThing, i);
 		}
-		int imunityvirus;
+		int imunityvirus = -1;;
 
 
 		//устанавливаем зомби скилл
 		for (i = 0; i < kolzombi; i++)
 		{	//перва€ попул€ци€. запоминаем мнеста заражени€
 			if (createvirus == 0)
-				population[i] = rand() % kolzombi;
+				population[i] = rand() % kolzombi +1;
 
 			zombiskill(TheThing, population[i], k1, k2, k3);
 		}
@@ -117,7 +123,7 @@ int main()
 			int j = kolzombi / 10;
 			while (j > 0)
 			{
-				zombiskill(TheThing, population[rand() % kolzombi], rand() % 101, rand() % 101, rand() % 101);
+				zombiskill(TheThing, population[rand() % kolzombi+1], rand() % 101+1, rand() % 101+1, rand() % 101+1);
 			}
 
 			//это просто селекци€, выживает сильнейший
@@ -140,8 +146,10 @@ int main()
 			}
 		}
 
+
+		int	endpole = 0;
 		//игра началась
-		while (kolhuman != 0 || kolzombi != 0)
+		while (kolhuman > 0 && kolzombi > 0)
 		{
 			for (i = 0; i < 1000; i++)
 			{
@@ -150,11 +158,19 @@ int main()
 				{
 					//радиус
 					for (int j = TheThing[i].x - TheThing[i].radius; j <= TheThing[i].x + TheThing[i].radius; j++)
+					{
+						if (endpole != 0) break;
 						for (int k = TheThing[i].y - TheThing[i].radius; k <= TheThing[i].y + TheThing[i].radius; k++)
 						{
 							//заражаем соседей
-							if (pole[j][k] != -1 && TheThing[pole[j][k]].health != 0)
+							endpole = 0;
+							if (k >= y) { k = y - 1; endpole = 1; }
+							if (j >= x) { j = x - 1;  endpole = 1; }
+							if (k < 0)	k = 0;
+							if (j < 0)	j = 0;
+							if (pole[j][k] != -1 && TheThing[pole[j][k]].health != 0 && pole[j][k] != i)
 								imunityvirus = TheThing[pole[j][k]].health - TheThing[i].virus;
+							else imunityvirus = -1;
 							if (imunityvirus >= 0)
 								TheThing[pole[j][k]].health -= imunityvirus;
 							//превращаем их в зомби
@@ -164,7 +180,9 @@ int main()
 								kolhuman--;
 								zombiskill(TheThing, pole[j][k], k1, k2, k3);
 							}
+							if (endpole != 0) break;
 						}
+					}
 					//конец радиуса
 					//уменьшаем жизнь зомби
 					if (--TheThing[i].life <= 0)
@@ -175,33 +193,45 @@ int main()
 				}//конец зомби
 				//люди, если здоровье минус - мЄртв.
 				else
+				{
 					//увеличиваем здоровье
 					if (TheThing[i].health > 0 && TheThing[i].health <= 95)
 					{
 						TheThing[i].health += 5;
 						if (TheThing[i].health > 100) TheThing[i].health = 100;
 					}
+					if (TheThing[i].health < 0)
+					{
+						TheThing[i].health = -100;
+						kolzombi--;
+					}
+				}
 				//конец люди
 			}
 
-			if (kolhuman <= 0)
-			{
-				printf("люди погибли.поздравл€ю");
-				if (kolzombi <= 0)
-				{
-					printf("но зомби тоже. создать новый вирус?");
-					int createvirus = 0;
-				}
-				int createvirus = 1;
-			}
+		}
+		if (kolhuman <= 0)
+		{
 
+			printf("people died. Congratulations\n");
 			if (kolzombi <= 0)
 			{
-				printf("зомби погибли. создание нового вируса");
-				int createvirus = -1;
+				printf("но зомби тоже. создать новый вирус? 0 - нет, 1 - да\n");
+				int s;
+				scanf("%d", &s);
+				if (s == 0) return 0;
+				else createvirus = -1;
 			}
+			return 0;
 		}
+
+		if (kolzombi <= 0)
+		{
+			printf("zombie killing. creating a new virus\n");
+			int createvirus = -1;
+		}
+
 	}
 
-		return 0;
-	}
+	return 0;
+}
